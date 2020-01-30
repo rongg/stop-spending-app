f152246ff4b2f9c13c22c2d6f5d46417633e1660
GaydaR <ronald.gayda.13@gmail.com>
2020-01-30 10:22:52 -0800

    Added normalize.css. Set default button color.

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import 'normalize.css';
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap/dist/js/bootstrap.min.js';
 import './styles/index.css';
