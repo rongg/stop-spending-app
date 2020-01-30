d4ac525910ed170e9b0bc8efa31e6c94d69ddafb
GaydaR <ronald.gayda.13@gmail.com>
2020-01-30 11:54:26 -0800

    Improve homepage

--- src/components/home/stop_spending_home.jsx
+++ src/components/home/stop_spending_home.jsx
@@ -9,7 +9,8 @@ function StopSpendingHome({user}) {
                 <div className={'row'}>
                     <div className={'col-12'}>
                         <PiggyBank width={275} height={175}/>
-                        <h4 style={{marginTop: '10px', marginBottom: '16px'}}>A habit-based personal expense logger.</h4>
+                        <h4 style={{marginTop: '10px', marginBottom: '16px'}}>A habit-based personal expense
+                            logger.</h4>
                         {!user && <a href={'/register'} className="btn btn-primary">Start Logging</a>}
                         {user && <a href={'/expense/new'} className="btn btn-primary">Log an Expense</a>}
                     </div>
@@ -21,23 +22,25 @@ function StopSpendingHome({user}) {
                 <p>
                     Stop Spending is a web application for anyone who desires a simple way to keep a log of their
                     daily expenses and view them over time. With it you can log expenses, create spending habits, set
-                    budgets, goals  and more.
+                    budgets and goals, log urges, and more.
                 </p>
             </div>
         </section>
         <section className={'features'}>
             <div className={'container'}>
                 <h2 className={'text-center'}><PiggyBank flip icon={'stop_sign.svg'}/> How to Stop Spending</h2>
-                <br/>
-                <br/>
+            </div>
+            <div className={'container'}>
                 <div className={'feature'}>
                     <div className={'row'}>
                         <div className={'col-12 col-md-4'}>
                             <h5>#1) Log Your Expenses Manually</h5>
-                            <p>Many expense tracking apps automate much of the logging process through integrations with your
-                                personal accounts such as bank, credit card, etc., thereby fighting the battle for you.</p>
-                            <p>The Stop Spending App doesn't need to know about any of these.</p>
-                            <p>Take ownership of your expenses by keeping an honest and accurate log of your wallet. It is half the battle!</p>
+                            <p>Many expense tracking apps automate much of the logging process through integrations with
+                                your personal accounts such as checking and savings linked to debit cards, credit cards,
+                                and loans etc., thereby fighting the battle for you.</p>
+                            <p>The Stop Spending App doesn't need to know about any of these!</p>
+                            <p>Take ownership of your expenses by keeping an honest and accurate log of your wallet. It
+                                is half the battle!</p>
                         </div>
                         <div className={'col-md-8 col-12 screen'}>
                             <div className={'card'}>
@@ -56,15 +59,17 @@ function StopSpendingHome({user}) {
                         </div>
                     </div>
                 </div>
-                <br/>
+            </div>
+            <div className={'container'}>
                 <div className={'feature'}>
-                    <br/>
                     <div className={'row'}>
                         <div className={'col-md-4 col-12'}>
                             <h5>#2) Name Your Spending Habits</h5>
                             <p>That $3 coffee in the morning is much more expensive than you think.</p>
                             <p>So put a label on it!</p>
-                            <p>Select an icon for your spending habit and name it. Habit names can be as general or specific as
+                            <p>Select an icon for your spending habit and name it. Habit names can be as general or
+                                specific
+                                as
                                 you like.</p>
                             <p> Some examples: </p>
                             <ul>
@@ -89,17 +94,19 @@ function StopSpendingHome({user}) {
                         </div>
                     </div>
                 </div>
-                <br/>
+            </div>
+            <div className={'container'}>
                 <div className={'feature'}>
-                    <br/>
                     <div className={'row'}>
                         <div className={'col-md-4 col-12'}>
                             <h5>#3) Set Budgets and Keep Logging</h5>
                             <p>
-                                Once you have identified your personal spending habits, it is time to log expenses accordingly.
+                                Once you have identified your personal spending habits, it is time to log expenses
+                                accordingly.
                             </p>
                             <p>
-                                Set monthly, weekly, or daily budgets. Assign need/want to expenses and log urges to spend.
+                                Set monthly, weekly, or daily budgets. Assign need/want to expenses and log urges to
+                                spend.
                             </p>
                         </div>
 
@@ -121,14 +128,15 @@ function StopSpendingHome({user}) {
                         </div>
                     </div>
                 </div>
-                <br/>
+            </div>
+            <div className={'container'}>
                 <div className={'feature'}>
-                    <br/>
                     <div className={'row'}>
                         <div className={'col-md-4 col-12'}>
-                            <h5>#4) Set Goals -- Micro-Budget, Abstain, Beat</h5>
+                            <h5>#4) Set Goals: Micro-Budget, Abstain, Beat</h5>
                             <p>
-                                Is someone's birthday coming up? Or any event that will inevitably cause you to spend more
+                                Is someone's birthday coming up? Or any event that will inevitably cause you to spend
+                                more
                                 than you would like on your habit? Want to stop spending completely?
                             </p>
                             <p>Set a goal to get over the hump!</p>
